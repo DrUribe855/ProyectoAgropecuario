54e59735ceeba0cfa382a489e55881af67936e25
destroyer012134 <miguelangelmorasena@gmail.com>
2023-10-31 11:14:31 -0500

    Adding class to project

---
+++ src/Clases/Vacas.java
@@ -0,0 +1,60 @@
+ package Clases;
+
+ 
+public class Vacas {
+
+    int id_vaca;
+    int id_finca;
+    String alias;
+    String descripcion;
+    String estado;
+
+    public int getId_vaca() {
+        return id_vaca;
+    }
+
+    public void setId_vaca(int id_vaca) {
+        this.id_vaca = id_vaca;
+    }
+
+    public int getId_finca() {
+        return id_finca;
+    }
+
+    public void setId_finca(int id_finca) {
+        this.id_finca = id_finca;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Vacas(int id_vaca, int id_finca, String alias, String descripcion, String estado) {
+        this.id_vaca = id_vaca;
+        this.id_finca = id_finca;
+        this.alias = alias;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+    
+}
--- src/proyecto/agropecuario/ProyectoAgropecuario.java
+++ src/proyecto/agropecuario/ProyectoAgropecuario.java
@@ -4,11 +4,7 @@ package proyecto.agropecuario;
 
 public class ProyectoAgropecuario {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
 }
